2187d74db6e53680743950168a74f74ba314d8a7
ruiz.terce <ruiz.terce@gmail.com>
2024-05-05 13:39:15 +0200

    Add form constraint validation and fix form labels

--- script.js
+++ script.js
@@ -90,21 +90,65 @@ addBookToLibrary(aHappyWorld);
 refreshBookShowcase();
 
 //Sidebar form to add new books
+
+//Custom constraint validation
+const bookTitle = document.querySelector("#bookTitle");
+bookTitle.setCustomValidity("Please enter a Title");
+bookTitle.addEventListener("input", () => {
+  if (bookTitle.validity.valueMissing) {
+    bookTitle.setCustomValidity("Please enter a Title");
+    bookTitle.reportValidity();
+  } else {
+    bookTitle.setCustomValidity("");
+  }
+});
+
+const bookAuthor = document.querySelector("#bookAuthor");
+bookAuthor.setCustomValidity("Please enter an Author");
+bookAuthor.addEventListener("input", () => {
+  if (bookAuthor.validity.valueMissing) {
+    bookAuthor.setCustomValidity("Please enter an Author");
+    bookAuthor.reportValidity();
+  } else {
+    bookAuthor.setCustomValidity("");
+  }
+});
+
+const bookPages = document.querySelector("#bookPages");
+bookPages.setCustomValidity("Please enter the number of pages");
+bookPages.addEventListener("input", () => {
+  if (bookPages.validity.valueMissing) {
+    bookPages.setCustomValidity("Please enter the number of pages");
+    bookPages.reportValidity();
+  } else if (bookPages.validity.rangeUnderflow) {
+    bookPages.setCustomValidity("Please enter a number greater than 1");
+    bookPages.reportValidity();
+  } else if (bookPages.validity.rangeOverflow) {
+    bookPages.setCustomValidity("Please enter a number lower than 1.000.000");
+    bookPages.reportValidity();
+  } else {
+    bookPages.setCustomValidity("");
+  }
+});
+
+//Submit form
 addBookForm = document.querySelector("#add-book-form");
 
 addBookForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  addBookToLibrary(
-    new Book(
-      addBookForm.bookTitle.value,
-      addBookForm.bookAuthor.value,
-      addBookForm.bookPages.value,
-      addBookForm.bookRead.checked
-    )
-  );
-  refreshBookShowcase();
-  addBookForm.reset();
-  document.querySelector("#add-book-sidebar").style = "display:none";
+  if (addBookForm.checkValidity()) {
+    addBookToLibrary(
+      new Book(
+        addBookForm.bookTitle.value,
+        addBookForm.bookAuthor.value,
+        addBookForm.bookPages.value,
+        addBookForm.bookRead.checked
+      )
+    );
+    refreshBookShowcase();
+    addBookForm.reset();
+    document.querySelector("#add-book-sidebar").style = "display:none";
+  }
 });
 
 document.querySelector("#close-sidebar-btn").addEventListener("click", () => {
